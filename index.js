7754dd109493b42e7f1e2e75a654dce061cde716
Christine Endriss <cendriss@gmail.com>
2018-08-28 22:36:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 window.addEventListener("load", () => {
   //start your code here
-let outputResult = document.querySelector("output")
-let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".split('')
-let upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
-let numberCharacters = "0123456789"
-let specialCharacters ="!#$%&()*+,-./:;<=>?@[\\]^_{|}~"
+  let outputResult = document.querySelector("output")
+  let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".split('')
+  let upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+  let numberCharacters = "0123456789".split('')
+  let specialCharacters = "!#$%&()*+,-./:;<=>?@[\\]^_{|}~".split('')
 
-  
-  document.addEventListener('submit' , (event) => {
-    //stay on the same page
-event.preventDefault();
-    
+  document.addEventListener('submit', (event) => {
+    // stay on the same page
+    event.preventDefault();
     
     let input = document.getElementById("passwordLength")
     let passwordLength = input.value
-    
-    
+    if (passwordLength < 4) {
+      passwordLength = 4
+    }
+    let newPassword = ""
+    for (let i = 0; i < passwordLength; i++;) {
+      
+    }
   })
-});
+  
+})
