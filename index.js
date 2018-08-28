a319174b3a8296eee544c7190227702d33924054
Christine Endriss <cendriss@gmail.com>
2018-08-28 22:35:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 window.addEventListener("load", () => {
   //start your code here
+let outputResult = document.querySelector("output")
+let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz".split('')
+let upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+let numberCharacters = "0123456789"
+let specialCharacters ="!#$%&()*+,-./:;<=>?@[\\]^_{|}~"
+
+  
+  document.addEventListener('submit' , (event) => {
+    //stay on the same page
+event.preventDefault();
+    
+    
+    let input = document.getElementById("passwordLength")
+    let passwordLength = input.value
+    
+    
+  })
 });
